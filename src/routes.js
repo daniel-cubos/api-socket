b60375934872771976e595d1be8f1d1f6e8dd6ae
Daniel <danieldeandradelopes@gmail.com>
2022-01-20 12:29:58 -0300

    create routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
-
+const MessageController = require('./apps/controllers/Message');
 const routes = Router();
 
 routes.get('/health', (req, res) => res.status(200).json('Health is ok.'));
+
+routes.get('/message', MessageController.allMessages);
+routes.post('/message', MessageController.sendMessage);
 
 module.exports = routes;
